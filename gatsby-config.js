8e7df919e4ce34063bd570627c10998f1bc3a5aa
Scott M Parrish <anithri@gmail.com>
2022-11-07 17:29:36 -0700

    remove fetchDownload settings for contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: process.env.CONTENTFUL_HOST,
-        downloadLocal: process.env.CONTENTFUL_DOWNLOAD
+        host: process.env.CONTENTFUL_HOST
       }
     },
     'gatsby-transformer-remark',
